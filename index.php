71824fb7828f7a1e1f4654a7c3a7e9351d5db638
kennyjacob <82275580+kennyjacob@users.noreply.github.com>
2021-04-10 15:35:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php 
+include("navbar.php");
+ ?>
+  <div class="container"> <!-- Slider start here -->
+                <div id="carousel-example-generic" class="carousel slide" data-mdb-ride="carousel">
+                      
+                        <div class="item">
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="img/s4.jpg" class="d-block w-100" alt="...">
+                            </div>
+
+                            <div class="carousel-item">
+                              <img src="img/s5.jpg" class="d-block w-100" alt="..."> 
+                            </div>
+
+                            <div class="carousel-item">
+                              <img src="img/s6.jpg" class="d-block w-100" alt="...">
+                            </div>
+                          </div>
+
+                          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                            data-mdb-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                            data-mdb-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </a>
+                        </div>
+                </div>
+              </div><!-- Slider Ends here -->
+<?php  
+include("picture.php");
+?>
+
+<?php
+include("test.php");
+?>
+
+              
+
+              <section class="">
+                  <!-- Footer -->
+                  <footer class="text-center text-white" style="background-color: #0a4275;">
+                    <!-- Grid container -->
+                    <div class="container p-4 pb-0">
+                      <img src="img/logo.png">
+                      <!-- Section: CTA -->
+                      
+                      <!-- Section: CTA -->
+                    </div>
+                    <!-- Grid container -->
+
+                    <!-- Copyright -->
+                    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                      © 2020 Copyright:
+                      <a class="text-white" href="https://mdbootstrap.com/">CAD International-All rights reserved</a>
+                    </div>
+                    <!-- Copyright -->
+                  </footer>
+                  <!-- Footer -->
+                </section>
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
